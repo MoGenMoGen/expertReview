4ed556d326cb317af78ff7c27150ee339a97331e
LAPTOP-0K1DKM9E\吴林超 <794826751@qq.com>
2021-10-13 14:43:07 +0800

    10.13

--- src/components/leftMenu.vue
+++ src/components/leftMenu.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="leftMenu">
     <el-menu
-      default-active="2"
+      default-active="0"
       unique-opened
       class="el-menu-vertical-demo"
       @select="handleSelect"
@@ -11,23 +11,23 @@
       text-color="#F8F8F8"
       active-text-color="#fff"
     >
-      <el-submenu index="1">
+	<el-menu-item index="0" :class="currentIndex=='0'?'activeTwo':''">
+	  <img src="../assets/img/总览-未选.png" style="width: 18px; height: 18px; margin-left: 5px;" v-if="currentIndex!='0'">
+	  <img src="../assets/img/总览-选中.png" style="width: 18px; height: 18px; margin-left: 5px;" v-if="currentIndex=='0'">
+	  <span slot="title" >总览</span>
+	</el-menu-item>
+      <el-submenu index="1"   >
         <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>导航一</span>
+          <img src="../assets/img/招标-未选.png" style="width: 18px; height: 18px; margin-left: 5px;" v-if="currentIndex!='1'&&currentIndex!='1-1'&&currentIndex!='1-2'&&currentIndex!='1-3'&&currentIndex!='1-4'">
+          <img src="../assets/img/招标-选中.png" style="width: 18px; height: 18px; margin-left: 5px;" v-if="currentIndex=='1'||currentIndex=='1-1'||currentIndex=='1-2'||currentIndex=='1-3'||currentIndex=='1-4'">
+          <span style="opacity: 0.5;" :class="currentIndex=='1'||currentIndex=='1-1'||currentIndex=='1-2'||currentIndex=='1-3'||currentIndex=='1-4'?'activeThree':''">招标</span>
         </template>
         <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
+          <el-menu-item index="1-1" :class="currentIndex=='1-1'?'active':''">项目立项</el-menu-item>
+          <el-menu-item index="1-2"  :class="currentIndex=='1-2'?'active':''">项目审批</el-menu-item>
+		  <el-menu-item index="1-3"  :class="currentIndex=='1-3'?'active':''">公告变更/澄清</el-menu-item>
+		  <el-menu-item index="1-4"  :class="currentIndex=='1-4'?'active':''">全部项目</el-menu-item>
         </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="1-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="1-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="1-4-1">选项1</el-menu-item>
-        </el-submenu>
       </el-submenu>
       <el-menu-item index="2">
         <i class="el-icon-menu"></i>
@@ -48,17 +48,30 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+		zhaobiao:[],
+		currentIndex:0,
+		currentIndexTwo:-1,
+		
+		
+		
+	};
   },
   mounted() {},
   methods: {
     handleSelect(key, keyPath) {
+		this.currentIndex=key
+		console.log('1233',this.currentIndex);
 		console.log(1111,key,keyPath);
 		if(key==2){
 			this.until.href('/views/index/onlineBidEvaluate.html')
 		}
+		if(key=='1-1'){
+			this.until.href('/views/index/zhaobiao.html?key='+key)
+		}
     },
     handleOpen(key, keyPath) {
+		this.currentIndex=key
       console.log(key, keyPath);
     },
     handleClose(key, keyPath) {
@@ -74,5 +87,48 @@ export default {
   width: 200px;
     height: calc(~"100vh - 298px");
   background-color: #2778be;
+	 .el-menu-item{
+		 font-size: 14px;
+		 font-weight: 400;
+		 color: #FFFFFF;
+		 opacity: 0.5;
+		 img{
+			 width: 18px;
+			 height: 18px;
+		 }
+	 }
+	 .active{
+		opacity: 1!important;
+		color: white !important;
+		background-color: rgb(31,96,152) !important;
+	 }
+	 .active::before{
+		 content: '';
+		 width: 3px;
+		 height: 14px;
+		 background: #F4F4F4;
+		  position: absolute;
+		  top: 50%;
+		  left: 30px;
+		  transform: translateY(-50%);
+	 }
+	 .activeTwo{
+		 opacity: 1!important;
+		 color: white !important;
+		 background-color: rgb(31,96,152) !important;
+	 }
+	 .activeThree{
+		 opacity: 1!important;
+		 color: white !important;
+	 }
+	 .el-menu-item:hover{
+		 font-size: 14px;
+		 font-weight: 400;
+		 color: #FFFFFF;
+		 opacity: 1;
+	 }
+	
+  
+ 
 }
 </style>
---
+++ src/views/index/zhaobiao/App.vue
@@ -0,0 +1,105 @@
+<template>
+    <div id="home" :style="{width:bWidth + 'px'}" v-loading="loading">
+		<my-header :width="width" :bWidth="bWidth"></my-header>
+		<div class="container" :style="{width:width + 'px'}">
+			<leftMenu></leftMenu>
+		</div>
+        <my-footer></my-footer>
+    </div>
+</template>
+
+<script>
+    import myFooter from '@/components/footer';
+	import myHeader from '@/components/myHeader';
+	import leftMenu from '@/components/leftMenu';
+    export default {
+        data(){
+            return{
+                loading:false,
+                bWidth:0,
+                width:0,
+                list:[],
+                pageNo:1,
+                pageSize:10,
+                total:0,
+				key:'',
+				
+            }
+        },
+        computed: {
+
+        },
+        components: {
+			myFooter,myHeader,leftMenu
+        },
+        async  mounted(){
+            // if(!this.until.seGet('userInfo')){
+            //     this.until.href('./login.html')
+            // }
+			console.log(this.navList)
+            this.getWidth()
+            // this.getList()
+            // this.userInfo = JSON.parse(this.until.seGet('userInfo'))
+            window.onresize = () => {
+                this.getWidth()
+            }
+			this.key=this.until.getQueryString('key')
+			console.log(this.key);
+			
+        },
+        methods:{
+            getWidth(){
+                let {bWidth,width} = this.until.getWidth()
+                this.bWidth = bWidth
+                this.width = width
+            },
+            toLogOut(){
+                this.api.logout()
+            },
+            //页面跳转
+            toPage(url){
+                this.until.href(url)
+            }
+        }
+    }
+</script>
+<style lang="less" scoped>
+    #home{
+        .tableHeader{
+            background: @themeColor;
+            color: #FFF;
+            border: 0;
+            text-align: center;
+        }
+        .rowList{
+            padding: 20px 0;
+        }
+    }
+</style>
+<style lang="less" scoped>
+    @import url("../../../assets/css/init.less");
+    #home{
+        background-color: #F3F3F3;
+        .blue{
+            color: @themeColor;
+        }
+        .red{
+            color: @redColor;
+        }
+        .projectNm{
+            cursor: pointer;
+        }
+    }
+	.container {
+		padding-top: 20px;
+		padding-bottom: 100px;
+		background: url(../../../assets/img/footer.png);
+		background-size: 100%;
+		background-repeat: no-repeat;
+		background-position: bottom center;
+		margin: 0 auto;
+	}
+    .gray{
+        color: #999999;
+    }
+</style>
