0dbba644af0109669360efb16d5a10350d617e15
yf <1060292708@qq.com>
2021-10-28 09:55:44 +0800

    部分修改

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,8 @@ var sequence = require('run-sequence');
 gulp.task('dev', function(){
     return gulp.src('wechat/**')
         .pipe(scp2({
-            host: '47.101.191.248',
-            // host: '47.101.167.43',
+            // host: '47.101.191.248',
+            host: '47.101.167.43',
             username: 'root',
             password: 'sinovat_ocean@2020',
             dest: '/usr/local/tomcat/sinovat/web/fb'
--- src/components/myEditor.vue
+++ src/components/myEditor.vue
@@ -26,8 +26,8 @@
 				  // UEditor 资源文件的存放路径，如果你使用的是 vue-cli 生成的项目，通常不需要设置该选项，vue-ueditor-wrap 会自动处理常见的情况，如果需要特殊配置，参考下方的常见问题2
 				  UEDITOR_HOME_URL:
 				    process.env.NODE_ENV === "production"
-				      ? "/static/ueditor/"
-				      : "/static/ueditor/"
+				      ? "/static/UEditor/"
+				      : "/static/UEditor/"
 				},
 			};
 		},
